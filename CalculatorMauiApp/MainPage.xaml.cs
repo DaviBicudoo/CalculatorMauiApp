559dceb9e503033c29f076f8cf570d5f73660eb0
Usuario <Usuario@CPD03-M13>
2024-03-25 15:58:08 -0300

    Adicionar arquivos de projeto.

---
+++ CalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿namespace CalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Clean_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MoreLess_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Percentage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Divide_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Multiply_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Subtract_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sum_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number0_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dot_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Equals_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
